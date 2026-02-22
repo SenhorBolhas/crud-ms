daa31bba84d50936548ae3a87aa04c905b087363
Giovanni <Giovanni_pelloso@hotmail.com>
2026-02-22 12:24:16 -0300

    update .gitignore Criando package entity Criando classe cliente

---
+++ src/main/java/org/example/entity/Cliente.java
@@ -0,0 +1,26 @@
+package org.example.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+}
