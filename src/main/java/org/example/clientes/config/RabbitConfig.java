a87803ca64169ab6ef84aa69e1217a7e1f941170
Giovanni <Giovanni_pelloso@hotmail.com>
2026-02-22 22:51:53 -0300

    Atualizando RabbitMQ

--- src/main/java/org/example/clientes/config/RabbitConfig.java
+++ src/main/java/org/example/clientes/config/RabbitConfig.java
@@ -4,26 +4,48 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitConfig {
 
+    public static final String EXCHANGE = "cliente.exchange";
+    public static final String QUEUE = "cliente.queue";
+    public static final String ROUTING_KEY = "cliente.criado";
+
     @Bean
-    public Queue queue() {
-        return new Queue("cliente.queue");
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
+                                         MessageConverter messageConverter) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter);
+        return template;
     }
 
     @Bean
     public TopicExchange exchange() {
-        return new TopicExchange("cliente.exchange");
+        return new TopicExchange(EXCHANGE);
+    }
+
+    @Bean
+    public Queue queue() {
+        return new Queue(QUEUE);
     }
 
     @Bean
     public Binding binding(Queue queue, TopicExchange exchange) {
-        return BindingBuilder.bind(queue)
+        return BindingBuilder
+                .bind(queue)
                 .to(exchange)
-                .with("cliente.criado");
+                .with(ROUTING_KEY);
     }
 }
